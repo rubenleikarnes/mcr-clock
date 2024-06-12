ad58b4ebd94ee0a95a59c44bfa286e750271e9e0
Ruben <ruben@2110.no>
2024-06-12 16:49:08 +0200

    added day of week

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const originalTime = new Date().toLocaleTimeString("nb-NO");
 const timeElm = document.getElementById("time")
 const dateElm = document.getElementById("date")
+const weekdayElm = document.getElementById("weekday")
 
 function updateTime() {
   const current = new Date();
+  const weekday = days[current.getDay()];
+  const month = months[current.getMonth()]
   const time = current.toLocaleTimeString("nb-NO");
   const date = current.toLocaleDateString("nb-NO");
+ 	const date2 = month + " " + current.getDate() + " " + current.getFullYear()
+
 
   timeElm.innerHTML = time;
-  dateElm.innerHTML = date;
+  dateElm.innerHTML = weekday + ", " + date2;
 }
 
 setInterval(updateTime,50);
